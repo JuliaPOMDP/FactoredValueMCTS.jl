4a91e275880d53bf52b948d87f4a8db0b817ddf0
rejuvyesh <mail@rejuvyesh.com>
2021-01-04 03:36:45 +0000

    add tests
    Won't work until other stuff registered first

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,68 @@
 using MAMCTS
 using Test
 
+using POMDPs
+using MultiAgentSysAdmin
+using MultiUAVDelivery
+
 @testset "MAMCTS.jl" begin
-    # Write your tests here.
+
+    @testset "varel" begin
+        @testset "sysadmin" begin
+
+            @testset "local" begin
+                mdp = BiSysAdmin()
+                solver = FVMCTSSolver()
+                planner = solve(solver, mdp)
+                s = rand(initialstate(mdp))
+                a = action(planner, s)
+                @test a isa actiontype(mdp)
+            end
+            
+
+            @testset "global" begin
+                mdp = BiSysAdmin(;global_rewards=true)
+                solver = FVMCTSSolver()
+                planner = solve(solver, mdp)
+                s = rand(initialstate(mdp))
+                a = action(planner, s)
+                @test a isa actiontype(mdp)
+            end
+        end
+    end    
+
+    @testset "maxplus" begin
+        @testset "sysadmin" begin
+
+            @testset "local" begin
+                mdp = BiSysAdmin()
+                solver = FVMCTSSolver(;coordination_strategy=MaxPlus())
+                planner = solve(solver, mdp)
+                s = rand(initialstate(mdp))
+                a = action(planner, s)
+                @test a isa actiontype(mdp)
+            end
+        
+
+            @testset "global" begin
+                mdp = BiSysAdmin(;global_rewards=true)
+                solver = FVMCTSSolver(;coordination_strategy=MaxPlus())
+                planner = solve(solver, mdp)
+                s = rand(initialstate(mdp))
+                a = action(planner, s)
+                @test a isa actiontype(mdp)
+            end
+        end
+
+        @testset "uav" begin
+            mdp = FirstOrderMultiUAVDelivery()
+            solver = FVMCTSSolver(;coordination_strategy=MaxPlus())
+            planner = solve(solver, mdp)
+            s = rand(initialstate(mdp))
+            a = action(planner, s)
+            @test a isa actiontype(mdp)
+        end
+
+    end
+
 end
